0171ae4568c43c5174e7477f1bb191d85516d526
faizalindrak <faizalindrak@gmail.com>
2020-07-14 21:47:39 +0700

    Chapter 4 Functios

---
+++ functions.py
@@ -0,0 +1,11 @@
+def greet(lang):
+    if lang == 'jawa':
+        print('sugeng rawuh')
+    elif lang == 'sunda':
+        print('mangga')
+    else:
+        print('selamat datang')
+
+greet('jawa')
+greet('sunda')
+greet('id')
