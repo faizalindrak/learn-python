fce3d00b00b52eac4848ae82024626c8ce3b2a53
faizalindrak <faizalindrak@gmail.com>
2020-07-14 20:14:07 +0700

    Working code

--- hitung-lembur-harian.py
+++ hitung-lembur-harian.py
@@ -2,38 +2,28 @@
 gaji = input("Masukkan gaji : ")
 hariKerja = input("Masukkan total hari kerja dalam satu minggu :")
 jamLembur = input("Masukkan total jam lembur hari ini :")
-rate = gaji
-salah = ""
-salah2 = ""
-salah3 = ""
 try:
-    int(rate)
+    gaji = float(gaji)
+    hariKerja = float(hariKerja)
+    jamLembur = float(jamLembur)
 except:
-    salah = "anda tidak memasukkan angka"
-try:
-    int(hariKerja)
-except:
-    salah2 = "anda tidak memasukkan angka"
-try:
-    int(jamLembur)
-except:
-    salah3 = "anda tidak memasukkan angka"
-print(salah,salah2,salah3)
+    print("tolong masukkan angka")
 
-if hariKerja == int(6):
-    print("aaa")
-    if jamLembur <= 7:
+rate = gaji/173
+
+if int(hariKerja) == int(6):
+    if int(jamLembur) <= 7:
         basicPay = jamLembur*rate*2
         outputPay = basicPay
-    elif jamLembur == 8:
+    elif int(jamLembur) == 8:
         basicPay = jamLembur*rate*2
         addPay = (jamLembur-7)*(rate*1)
         outputPay = basicPay + addPay
-    elif jamLembur >= 9:
+    elif int(jamLembur) >= 9:
         basicPay = jamLembur*rate*2
         addPay = (jamLembur-7)*(rate*1)
         morePay = (jamLembur-8)*(rate*1)
         outputPay = basicPay+addPay+morePay
 else:
     outputPay = "error"
-print (outputPay)
+print (int(outputPay))
